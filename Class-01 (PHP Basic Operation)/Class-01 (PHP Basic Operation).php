e5459dbe5bb755e1ededfe4dd6d84bf7e07f7984
Moni Kishore Dhar <76126980+moni-kishore-dhar@users.noreply.github.com>
2021-09-08 19:51:34 +0600

    Create Class-01 (PHP Basic Operation).php

---
+++ Class-01 (PHP Basic Operation)/Class-01 (PHP Basic Operation).php
@@ -0,0 +1,88 @@
+<?php
+
+
+echo"<title>Class-01 (php basic)</title>";
+
+// echo "hello";
+// echo "<br>";
+
+
+
+
+
+		//Variable
+
+
+
+// $a=7;
+// $b=3;
+
+// $addition=$a+$b;
+// echo $addition."<br>";
+
+// $summation=$a-$b;
+// echo $summation."<br>";
+
+// $reminder=$a%$b;
+// echo $reminder."<br>";
+
+// $multiplication=$a*$b;
+// echo $multiplication."<br>";
+
+
+// $a=9.9;
+// echo $a."<br>";
+
+// $string="kd gh";
+// echo $string."<br>";
+
+// //Boolian 
+// $flag=true;
+// echo $flag."<br>";
+
+
+// $a=7;
+// $a++;
+// $a++;
+
+// echo $a;
+
+// $b=5;
+// $b+=3;
+// echo $b;
+
+
+
+
+
+		//Logiacl Operaor
+
+
+
+
+// $a=10;
+// if($a%2==0)
+// 	{
+// 		echo $a." is an even number";
+// 	}
+// else{
+// 		echo $a." is an odd number";
+// 	}
+
+
+
+
+
+
+
+
+
+echo "<h1>Hello</h1><br><p>This is a paragraph</p>";  
+
+
+
+
+
+
+
+?>
